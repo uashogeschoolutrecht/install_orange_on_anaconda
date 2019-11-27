476772180af7c93d0268b1aea0478031ce1e6e59
Marc <marc.teunis@hu.nl>
2019-11-27 09:02:05 +0100

    Add files via upload

---
+++ install_anaconda.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# get latest distro at
+# https://www.anaconda.com/distribution/
+# https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
+
+cd /tmp
+curl -O https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
+bash Anaconda3-2019.10-Linux-x86_64.sh
+
