94a8b6da112543398f87dfb45951c8dc50e74dd5
Marc <marc.teunis@hu.nl>
2019-11-27 11:14:59 +0100

    Update install_anaconda.sh

--- install_anaconda.sh
+++ install_anaconda.sh
@@ -5,7 +5,7 @@
 # https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
 
 curl -O 'https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh'
-bash -y Anaconda3-2019.10-Linux-x86_64.sh
+bash Anaconda3-2019.10-Linux-x86_64.sh
 
 rm Anaconda3-2019.10-Linux-x86_64.sh
 source ~/.bashrc
