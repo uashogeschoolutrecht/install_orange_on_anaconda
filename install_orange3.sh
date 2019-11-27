38c80d86ab53f406dd13babf81268daeba019285
Marc <marc.teunis@hu.nl>
2019-11-27 09:01:42 +0100

    Create install_orange3.sh

---
+++ install_orange3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## get orange on anaconda with conda
+
+conda config --add channels conda-forge
+conda install orange3
+conda install -c defaults pyqt=5 qt
+
+## run with
+# python -m Orange.canvas
